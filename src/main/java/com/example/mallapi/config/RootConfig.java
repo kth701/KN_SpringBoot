19b87801298f38a4cbc3b9696209022bccda500d
kth <kth701@nate.com>
2025-09-05 20:30:35 +0900

    modelmapper

---
+++ src/main/java/com/example/mallapi/config/RootConfig.java
@@ -0,0 +1,26 @@
+package com.example.mallapi.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class RootConfig {
+
+    public ModelMapper modelMapper () {
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration()
+            .setFieldMatchingEnabled(true)// 필드 맵핑 활성화
+            .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)//  private 수준
+            .setMatchingStrategy(MatchingStrategies.LOOSE);
+
+        return modelMapper; // DTO -> Entity, Entity -> DTO 역할
+    }
+
+
+}
