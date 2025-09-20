2b7178144bd4391d72b63692b5f1dee4b082aa5f
kth701 <kth701@nate.com>
2025-09-20 13:47:37 +0900

    test

--- src/main/java/com/example/mallapi/mall/repository/MemberRepository.java
+++ src/main/java/com/example/mallapi/mall/repository/MemberRepository.java
@@ -21,4 +21,6 @@ public interface MemberRepository extends JpaRepository<Member, String> {
 JPQL의 페치 조인(fetch join)은 연관된 엔티티나 컬렉션을 한 번의 SQL 쿼리로 함께 조회하여 성능을 최적화하는 기능
 Member를 조회할 때 memberRolesList도 같이 조회
 Repository의 메서드에 @EntityGraph를 붙여 fetch join을 적용
+
+test
  */
