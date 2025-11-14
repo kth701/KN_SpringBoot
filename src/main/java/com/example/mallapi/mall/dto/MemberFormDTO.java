8f89eea424e3bd62a6cfa9bc1093306cafb25a31
kth <kth701@nate.com>
2025-11-14 15:04:53 +0900

    회원관리 수정 작업:권한 설정

--- src/main/java/com/example/mallapi/mall/dto/MemberFormDTO.java
+++ src/main/java/com/example/mallapi/mall/dto/MemberFormDTO.java
@@ -23,11 +23,18 @@ public class MemberFormDTO {
 
     @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
     @Length(min=4, max=16, message="비밀번호는 4자이상, 16자 이하로 입력해주세요")
-    private String pw;
+    private String pw; // 새비밀번호(입력폼에서는 비밀번호)
 
-    private String currentPw;
-    private String confirmPw;
-    private String savedPw;
+    /*
+    1. 비밀번호 변경 테스트용 :
+    호원 수정 작업 폼에서 기존 비밀번호 입려 확인후 맞으면 새비밀번호 입력 확인
+     */
+    /* --------------------------------------------------------------------------------------- */
+    private String currentPw; // 기존 비밀번호 입력
+    private String confirmPw; // 비밀번호 확인
+    private String savedPw; // DB에 저장된 기존 비밀번호
+    /* --------------------------------------------------------------------------------------- */
+
 
     @NotBlank(message = "닉네임은 필수 입력 값입니다.")
     private String nickname;
